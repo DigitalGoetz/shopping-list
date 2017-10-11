f69f0570f718d27dbe793ef4e4dbd66da6293cc5
Michael Goetz <michael.goetz@altamiracorp.com>
2017-10-11 16:10:56 -0400

    Included Swagger Documentation

--- src/main/java/digital/goetz/shopping/web/ShoppingBackendController.java
+++ src/main/java/digital/goetz/shopping/web/ShoppingBackendController.java
@@ -7,46 +7,48 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import digital.goetz.shopping.data.Item;
 import digital.goetz.shopping.data.ItemRepository;
 import digital.goetz.shopping.data.State;
 
-@Controller
+@RestController
+@RequestMapping("/api")
 public class ShoppingBackendController {
 
 	@Autowired
 	ItemRepository itemRepository;
 
-	@RequestMapping("/")
-	@ResponseBody
-	String home() {
-		return "test";
-	}
-
-	@RequestMapping("/listall")
+	@RequestMapping(path = "/list/all", method = RequestMethod.GET)
 	@ResponseBody
 	ResponseEntity<List<Item>> getAll() {
 		List<Item> list = new ArrayList<>();
 		itemRepository.findAll().forEach(item -> list.add(item));
 		return ResponseEntity.ok(list);
 	}
-	
-	@RequestMapping("/buylist")
+
+	@RequestMapping(path = "/list/buy", method = RequestMethod.GET)
 	@ResponseBody
-	ResponseEntity<List<Item>> getList() {
+	ResponseEntity<List<Item>> getBuy() {
 		List<Item> list = new ArrayList<>();
 		itemRepository.findByState(State.BUY).forEach(item -> list.add(item));
 		return ResponseEntity.ok(list);
 	}
-	
+
+	@RequestMapping(path = "/list/bought", method = RequestMethod.GET)
+	@ResponseBody
+	ResponseEntity<List<Item>> getBought() {
+		List<Item> list = new ArrayList<>();
+		itemRepository.findByState(State.BOUGHT).forEach(item -> list.add(item));
+		return ResponseEntity.ok(list);
+	}
 
 	@RequestMapping(path = "/item", method = RequestMethod.POST)
 	@ResponseBody
